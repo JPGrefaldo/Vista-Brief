319ddabc166bee4cc2517f86a141dc27bf86e32a
rayromero <ray.romero@objective.agency>
2016-10-03 12:30:44 +0800

    brand color

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,37 +36,20 @@ Dashboard
           <h1 class="m-n font-thin h3 text-black">Dashboard</h1>
           <small class="text-muted">welcome</small>
         </div>
-        <div class="col-sm-6 text-right hidden-xs hide">
-          <div class="inline m-r text-left">
-            <div class="m-b-xs">1290 <span class="text-muted">items</span></div>
-            <div ng-init="d3_1=[ 106,108,110,105,110,109,105,104,107,109,105,100,105,102,101,99,98 ]" 
-              ui-jq="sparkline" 
-              ui-options="[ 106,108,110,105,110,109,105,104,107,109,105,100,105,102,101,99,98 ], {type:'bar', height:20, barWidth:5, barSpacing:1, barColor:'#dce5ec'}" 
-              class="sparkline inline">loading...
-            </div>
-          </div>
-          <div class="inline text-left">
-            <div class="m-b-xs">$30,000 <span class="text-muted">revenue</span></div>
-            <div ng-init="d3_2=[ 105,102,106,107,105,104,101,99,98,109,105,100,108,110,105,110,109 ]" 
-              ui-jq="sparkline" 
-              ui-options="[ 105,102,106,107,105,104,101,99,98,109,105,100,108,110,105,110,109 ], {type:'bar', height:20, barWidth:5, barSpacing:1, barColor:'#dce5ec'}" 
-              class="sparkline inline">loading...
-            </div>
-          </div>
-        </div>
       </div>
     </div>
     <!-- / main header -->
-    <div class="wrapper-md" ng-controller="FlotChartDemoCtrl">
+
+    <div class="wrapper-md">
       <!-- Main Actions -->
       <div class="row">
   	    <div class="col-lg-6 col-md-6 col-sm-6">
   	      <div class="panel b-a">
-  	        <div class="panel-heading text-center bg-info no-border">
+  	        <div class="panel-heading text-center bg-brand-1 no-border">
   	          <h4 class="text-u-c m-b-none hide">Planning Requests</h4>
   	          <h2 class="m-t-none">
   	            <sup class="pos-rlt hide" style="top:-22px">$</sup>
-  	            <span class="h3 m-t-xs mb-x-s text-lt">Planning Requests</span>
+  	            <span class="h3 m-t-xs mb-x-s text-white">Planning Requests</span>
   	            <span class="text-xs hide">/ mo</span>
   	          </h2>
   	        </div>
@@ -79,17 +62,17 @@ Dashboard
   	          </li>
   	        </ul>
   	        <div class="panel-footer text-center">
-  	          <a href class="btn btn-info block font-bold m">Create new Planning Request</a>
+  	          <a href class="btn bg-brand-1 text-white block font-bold m">Create new Planning Request</a>
   	        </div>
   	      </div>
   	    </div>
   	    <div class="col-lg-6 col-md-6 col-sm-6">
   	      <div class="panel b-a">
-  	        <div class="panel-heading text-center bg-info no-border">
+  	        <div class="panel-heading text-center bg-brand-1 no-border">
   	          <h4 class="text-u-c m-b-none hide">Brief Sheets</h4>
   	          <h2 class="m-t-none">
   	            <sup class="pos-rlt hide" style="top:-22px">$</sup>
-  	            <span class="h3 m-t-xs mb-x-s text-lt">Brief Sheets</span>
+  	            <span class="h3 m-t-xs mb-x-s text-white">Brief Sheets</span>
   	            <span class="text-xs hide">/ mo</span>
   	          </h2>
   	        </div>
@@ -102,7 +85,7 @@ Dashboard
   	          </li>
   	        </ul>
   	        <div class="panel-footer text-center">
-  	          <a href class="btn btn-info block font-bold m">Create new Brief</a>
+  	          <a href class="btn bg-brand-1 text-white block font-bold m">Create new Brief</a>
   	        </div>
   	      </div>
   	    </div>
--- resources/views/includes/mainmenu-left.blade.php
+++ resources/views/includes/mainmenu-left.blade.php
@@ -5,39 +5,35 @@
           <nav ui-nav class="navi clearfix">
             <ul class="nav">
               <li class="hidden-folded padder m-t m-b-sm text-white text-xs">
-                <span>Menu</span>
+                <span></span>
               </li>
               <li>
                 <a href="{{ url('dashboard') }}">
-                  <i class="glyphicon glyphicon-stats icon text-info-lter"></i>
+                  <i class="glyphicon glyphicon-stats icon text-white"></i>
                   <span class="text-white">Dashboard</span>
                 </a>
               </li>
               <li>
                 <a href="{{ route('briefs') }}">
-                  <b class="badge bg-info pull-right">9</b>
-                  <i class="glyphicon glyphicon-envelope icon text-info-lter"></i>
+                  <i class="glyphicon glyphicon-envelope icon text-white"></i>
                   <span class="text-white">Brief Sheets</span>
                 </a>
               </li>
               <li>
                 <a href="">
-                  <b class="badge bg-info pull-right">9</b>
-                  <i class="glyphicon glyphicon-envelope icon text-info-lter"></i>
+                  <i class="glyphicon glyphicon-envelope icon text-white"></i>
                   <span class="text-white">Planning Requests</span>
                 </a>
               </li>
               <li>
                 <a href="{{ url('users') }}">
-                  <b class="badge bg-info pull-right">9</b>
-                  <i class="glyphicon glyphicon-envelope icon text-info-lter"></i>
+                  <i class="glyphicon glyphicon-envelope icon text-white"></i>
                   <span class="text-white">Users</span>
                 </a>
               </li>
               <li>
                 <a href="">
-                  <b class="badge bg-info pull-right">9</b>
-                  <i class="glyphicon glyphicon-envelope icon text-info-lter"></i>
+                  <i class="glyphicon glyphicon-envelope icon text-white"></i>
                   <span class="text-white">Departments</span>
                 </a>
               </li>
