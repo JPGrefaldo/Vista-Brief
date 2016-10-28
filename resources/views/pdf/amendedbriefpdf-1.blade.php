fa0e342d02cb8084b1e9264556af480bbd25444e
Ray Romero <raromero2009@gmail.com>
2016-10-28 22:57:57 +0800

    update pdf color chaning

--- resources/views/pdf/amendedbriefpdf-1.blade.php
+++ resources/views/pdf/amendedbriefpdf-1.blade.php
@@ -4,6 +4,13 @@
 Brief Submitted File
 @endsection
 
+<?php
+if ($brief->projectstatus_id == 1) {
+  $ps_color = "background-color:#f2dede;color:#a94442";
+} else {
+  $ps_color = "background-color:#d9edf7;color:#31708f";
+}
+?>
 
 @section('content')
 <div class="page-break">
@@ -31,7 +38,7 @@ Brief Submitted File
         </div>
         <div class="col-xs-6">
           <label class="control-label text-left"><strong>Project Status</strong></label>
-        	<p class="bg-light p-l-sm">{{ $brief->projectstatus->name }}&nbsp;</p>
+        	<p class="bg-light p-l-sm" style="{{$ps_color}}">{{ $brief->projectstatus->name }}&nbsp;</p>
         </div>
 
         <div class="col-xs-6">
@@ -102,7 +109,7 @@ Brief Submitted File
     <div class="col-xs-12">
       <div class="row m-b-md">
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>Amends</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>Amends</strong></p>
         </div>
 
         @foreach ($brief->amendments_desc as $key => $amend)
@@ -166,14 +173,14 @@ Brief Submitted File
 
         @if ($brief->summary)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#01 Brief Summary</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#01 Brief Summary</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->summary }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->disciplines_required_ids)
         <div class="col-xs-12">
-          <p class="bg-primary p-l-sm"><strong>#02 Disciplines Required</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#02 Disciplines Required</strong></p>
         </div>
         <div class="col-xs-12 m-b-lg">
           <div class="row">
@@ -191,63 +198,63 @@ Brief Submitted File
 
         @if ($brief->objectives_or_measures)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#03 Objectives / Measure</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#03 Objectives / Measure</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->objectives_or_measures }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->content)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#04 Context</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#04 Context</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->content }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->targetaudience_and_insight)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#05 Target Audience and Insight</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#05 Target Audience and Insight</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->targetaudience_and_insight }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->targetaudience_think)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#06 What do I want the target audience to Think?</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#06 What do I want the target audience to Think?</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->targetaudience_think }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->targetaudience_feel)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#06 What do I want the target audience to Feel?</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#06 What do I want the target audience to Feel?</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->targetaudience_feel }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->targetaudience_do)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#06 What do I want the target audience to Do?</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#06 What do I want the target audience to Do?</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->targetaudience_do }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->keymessages_or_propositions)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#07 Key Messages / Propositions</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#07 Key Messages / Propositions</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->keymessages_or_propositions }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->creative)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#08 Creative</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#08 Creative</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->creative }}&nbsp;</p>
         </div>
         @endif
 
         @if ($brief->budget_timings_and_outputs)
         <div class="col-xs-12 m-b-lg">
-          <p class="bg-primary p-l-sm"><strong>#09 Budgets, Timings and Outputs Required</strong></p>
+          <p class="bg-primary p-l-sm" style="{{$ps_color}}"><strong>#09 Budgets, Timings and Outputs Required</strong></p>
           <p class="bg-light p-l-sm">{{ $brief->budget_timings_and_outputs }}&nbsp;</p>
         </div>
         @endif
