6392badafff906c5a51eda737ffb628e4799c996
Ray Romero <raromero2009@gmail.com>
2016-11-21 17:45:42 +0800

    updates some pages;

--- app/Http/Requests/UpdateBriefRequest.php
+++ app/Http/Requests/UpdateBriefRequest.php
@@ -26,16 +26,23 @@ class UpdateBriefRequest extends FormRequest
     public function rules()
     {
         return [
-            'client'        =>  'required',
-            'projectstatus' =>  'required',
-            'jobnumber'     =>  'required',
-            'jobname'       =>  'required|unique:briefs,jobname,'.$this->brief_id,
-            'quotereq'      =>  'required_without_all:proposedreq,stagereq,projdelivered',
-            'proposedreq'   =>  'required_without_all:quotereq,stagereq,projdelivered',
-            'stagereq'      =>  'required_without_all:quotereq,proposedreq,projdelivered',
-            'projdelivered' =>  'required_without_all:quotereq,proposedreq,stagereq',
-            'department'    =>  'required',
-            'attachments.*' =>  'max:5120',
+            'client'            =>  'required',
+            'projectstatus'     =>  'required',
+            'jobnumber'         =>  'required|max:6',
+            'oldjobnumber'      =>  'max:6',
+            'budget'            =>  'required|max:50',
+            'pmanager'          =>  'required',
+            'jobname'           =>  'required|max:75|unique:briefs,jobname,'.$this->brief_id,
+            'keydeliverables'   =>  'required|max:75',
+            'quotereq'          =>  'required',
+            'proposedreq'       =>  'required',
+            'stagereq'          =>  'required',
+            'projdelivered'     =>  'required',
+            'summary'           =>  'required|max:200',
+            'department'        =>  'required',
+            'objmeasure'        =>  'required',
+            'budget_timings_outputs_req'    =>  'required',
+            'attachments.*'     =>  'max:5120',
         ];
     }
 
@@ -43,16 +50,27 @@ class UpdateBriefRequest extends FormRequest
     {
         return [
             'client.required'           =>  'Client is required.',
-            'projectstatus.required'    =>  'Project Status is requried.',
+            'projectstatus.required'    =>  'Project Status is required.',
             'jobnumber.required'        =>  'Job Number is required.',
+            'oldjobnumber.max'          =>  'Old Job Number may not be greater than 6 characters.',
+            'budget.required'           =>  'Budget is required.',
+            'budget.max'                =>  'Budget may not be greater than 50 characters.',
+            'pmanager.required'         =>  'Project Manager is required.',
             'jobname.required'          =>  'Job Name is required.',
-            'jobname.unique'            =>  'That Job Name is already taken.',
-            'quotereq.required_without_all'  =>  'You need to choose at least one of the required dates.',
-            'proposedreq.required_without_all'  =>  'You need to choose at least one of the required dates.',
-            'stagereq.required_without_all'  =>  'You need to choose at least one of the required dates.',
-            'projdelivered.required_without_all'  =>  'You need to choose at least one of the required dates.',
+            'jobname.max'               =>  'Job Name may not be greater than 75 characters.',
+            'jobname.unique'            =>  'Job Name is already taken.',
+            'keydeliverables.required'  =>  'Key Deliverables is required.',
+            'keydeliverables.max'       =>  'Key Deliverables may not be greater than 75 characters.',
+            'quotereq.required'         =>  'Quote Required by is required.',
+            'proposedreq.required'      =>  'Proposed Required by is required.',
+            'stagereq.required'         =>  '1st Stage Required by is required.',
+            'projdelivered.required'    =>  'Projects Delivered by is required.',
+            'summary.required'          =>  'Summary is required.',
+            'summary.max'               =>  'Summary may not be greater than 200 characters.',
             'department.required'       =>  'You need to select at least one discipline.',
-            'attachments.*'               =>  'Each attached file must not exceed 5MB of size.',
+            'objmeasure.required'       =>  'Objectives / Measure is required.',
+            'budget_timings_outputs_req.required'   =>  'Budget, Timings and Outputs Required is required.',
+            'attachments.*'             =>  'Each attached file must not exceed 5MB of size.',
         ];
     }
 
