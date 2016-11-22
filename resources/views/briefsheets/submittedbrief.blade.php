da5728b87316d714d3485b477d79d852ec118302
Ray Romero <raromero2009@gmail.com>
2016-11-22 19:24:56 +0800

    update

--- resources/views/briefsheets/submittedbrief.blade.php
+++ resources/views/briefsheets/submittedbrief.blade.php
@@ -333,8 +333,7 @@ Submitted - Brief Sheet
                       class="form-control auto-height" 
                       style="overflow:hidden;min-height:100px;" 
                       placeholder="Type new amend here.." 
-                    >{{ old('content') }}
-                    </textarea>
+                    >{{ old('content') }}</textarea>
                   </div>
                 </div>
 
