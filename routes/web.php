785f614d66717f0e846edcb5c0543ee7023ea05c
Ray Romero <raromero2009@gmail.com>
2016-12-05 17:23:23 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -128,7 +128,7 @@ Route::group(['middleware' => 'auth'], function() {
 
 	/* PDF Generator: Test Controller */
 	Route::get('/pdf/file/{id}', [
-		'uses'	=> 'PdfController@get_pdfSubmittedPlanningAttachment',
+		'uses'	=> 'PdfController@get_pdfSubmittedBriefAttachment',
 		'as'	=>	'pdfbriefsubmit'
 	]);
 	/* / PDF Generator */
