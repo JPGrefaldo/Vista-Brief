ce6479a6da0b826bbf9292e462e770c2617fde31
Ray Romero <raromero2009@gmail.com>
2016-12-15 16:45:05 +0800

    update

--- resources/views/briefsheets/newbrief.blade.php
+++ resources/views/briefsheets/newbrief.blade.php
@@ -399,7 +399,7 @@ Create New Brief Sheet
                       name="objmeasure" 
                       class="form-control auto-height" 
                       style="min-height:120px;" 
-                      placeholder="*What does the client want to achieve?&#x09;&#10;*Why?&#x09;&#10;*What difference will that make to their business / audience / etc?&#x09;&#10;*What does success looks like?&#x09;&#10;*How will it be measured?"
+                      placeholder="*What does the client want to achieve?&#x0a;*Why?&#x0a;*What difference will that make to their business / audience / etc?&#x0a;*What does success looks like?&#x0a;*How will it be measured?"
                     >{{ old('objmeasure') }}</textarea>
                   </div>          
                 </div>
